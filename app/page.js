8053132c14e69b1d76cacf4419c85ecf97c7eb7f
nowebcl <contacto@micelioconstruccion.cl>
2026-06-25 23:32:55 -0400

    Merge footer copyright background, scale up white logo, and replace text links with premium SVG social icons

--- app/page.js
+++ app/page.js
@@ -1708,24 +1708,39 @@ export default function Home() {
           </div>
         </div>
 
-        {/* Bottom copyright bar */}
+        {/* Bottom copyright bar - Blends seamlessly with footer above */}
         <div className="copyright-wrap">
           <div className="container">
             <div className="copyright-row">
-              {/* Silver blended logo */}
+              {/* Silver blended logo - Larger & Prominent */}
               <Image 
                 src="/assets/logo_corporativo.png" 
                 alt="Micelio Logo" 
-                width={130} 
-                height={40} 
+                width={180} 
+                height={55} 
+                priority
               />
               <div className="copy-text">
                 Copyright &copy; 2026 MICELIO INGENIERÍA Y CONSTRUCCIÓN. Todos los derechos reservados.
               </div>
+              
+              {/* High-Fidelity Professional SVG Social Icons */}
               <div className="footer-social">
-                <a href="#inicio">In</a>
-                <a href="#inicio">Ig</a>
-                <a href="#inicio">Fb</a>
+                <a href="#inicio" title="LinkedIn" aria-label="LinkedIn">
+                  <svg width="16" height="16" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.79M6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37h2.77z" />
+                  </svg>
+                </a>
+                <a href="#inicio" title="Instagram" aria-label="Instagram">
+                  <svg width="16" height="16" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.051C.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 1 0 0 12.324 6.162 6.162 0 0 0 0-12.324zM12 16a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm6.406-11.845a1.44 1.44 0 1 0 0 2.881 1.44 1.44 0 0 0 0-2.881z" />
+                  </svg>
+                </a>
+                <a href="#inicio" title="Facebook" aria-label="Facebook">
+                  <svg width="16" height="16" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.8c4.56-.93 8-4.96 8-9.8z" />
+                  </svg>
+                </a>
               </div>
             </div>
           </div>
